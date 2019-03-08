31ae4a1939bdda33afa1e14b023c6c2c8255c3ce
cat of Mr Lu <lxxlxx8888@outlook.com>
2019-03-08 11:44:02 +0800

    second commit

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,23 @@
-
+# 语法练习
 print("hello world");
 
 print(12 + 31);
 
-print("我是鲁新新");
+a= 12;
+b= 23;
+
+# python句尾不用分号
+print(a + b)
+
+print("我是鲁新新")
+
+
+counter = 100  # 赋值整型变量
+miles = 1000.0  # 浮点型
+name = "John"  # 字符串
+
+print (counter)
+print (miles)
+print(name)
+
+
